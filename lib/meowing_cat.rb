78f9256683543431f38e534644a27e887d0dcbdd
github username <github email address>
2019-12-11 17:12:45 -0500

    stashing results

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
 ## code your solution here. 
+
+class Cat
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def meow
+        puts "meow!"
+    end
+
+end
+
+stella = Cat.new("Stella")
+stella.meow
+stella.name
